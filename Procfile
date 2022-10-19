8bf5c760b4c908ca9bb6070c796c0c57445dd403
sa30h <satishrjk07@gmail.com>
2022-10-19 18:26:19 +0530

    heroku added

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn stuc.wsgi --log-file=-
--- stuc/settings.py
+++ stuc/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+from threading import local
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,10 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR,'static')
 ]
+STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
+
+
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
