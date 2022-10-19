5633c83dd7a85013e551d5cff125b536d2be2695
sa30h <satishrjk07@gmail.com>
2022-10-19 19:23:07 +0530

    psycopg removed

--- stuc/settings.py
+++ stuc/settings.py
@@ -126,12 +126,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR,'static')
-]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR,'static')
+# ]
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../myapp/static')
+)
 
 django_heroku.settings(locals())
 
