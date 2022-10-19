0f7c2acbcb6d9dc8034e26c616aa3b29e590912c
sa30h <satishrjk07@gmail.com>
2022-10-19 19:41:32 +0530

    psycopg removed

--- stuc/urls.py
+++ stuc/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,10 +30,9 @@ urlpatterns = [
 
 
 
-]
-# +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if not settings.DEBUG:
-    urlpatterns +=('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+# if not settings.DEBUG:
+#     urlpatterns +=('',
+#         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+#     )
